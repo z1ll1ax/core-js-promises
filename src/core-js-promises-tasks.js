f0603f84aa2de75eb6d661a380cfd4df8b812900
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2025-03-24 16:24:03 +0300

    7/7 solved

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -119,8 +119,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((value) => {
+      return value.status === 'fulfilled' ? value.value : null;
+    })
+  );
 }
 
 /**
@@ -141,8 +145,10 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((acc, promise) => {
+    return acc.then((result) => promise.then((value) => result + value));
+  }, Promise.resolve(''));
 }
 
 module.exports = {
